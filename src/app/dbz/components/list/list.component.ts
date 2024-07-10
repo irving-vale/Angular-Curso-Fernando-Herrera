858794b91dcef7d9df7c5b5d453de4e9914b879c
Irving Valencia <75141924+irving-vale@users.noreply.github.com>
2024-07-10 14:32:05 -0600

    se termino la seccion 5 del curso se vcieron temas como depuracion, metodo de anadir personaje con el @Output creando un EventEmitter, trabajamos con servicios para separar la logica de negocio, incluimos paqueteria UUID para agregar id, se modifico metodos para que del main se jalara los del servicio y se anotaranb en el html

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,18 +1,32 @@
-import { Component, Input,} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
+import { identity } from 'rxjs';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
-export class ListComponent{
+export class ListComponent {
+  @Input()
+  public characterList: Character[] = [
+    // se esta jalando los valores de main.page.component.ts y se esta mostrando en list.component.html
+    // por medio de la anotacion @Input()
+  ];
 
-    @Input()
-    public characterList: Character[] = [
+  @Output()
+  oneDelete: EventEmitter<string> = new EventEmitter();
 
-      // se esta jalando los valores de main.page.component.ts y se esta mostrando en list.component.html
-      // por medio de la anotacion @Input()
-    ];
+  // EventEmitter <T> oneDelete = new EventEmitter<>(); es como construir un objeto en java
 
+  oneDeleteCharacter(id?: string): void {
+    // TODO: Emitir el ID del personaje a eliminar
+    if (!id) return;
+
+    this.oneDelete.emit(id);
+  }
+
+  // oneDeleteCharacter(index:number):void{
+  //   this.characterList.splice(index, 1);
+  // }
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
-import { Character } from './../interfaces/character.interface';
-import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,23 +9,20 @@ import { Component } from '@angular/core';
 
 export class MainPageComponent {
 
-  public characters: Character[] = [{
-  name: 'Krillin',
-  power: 1000
- },
-{
-  name: 'Goku',
-  power: 9500
-},
-{
-  name:'Vegeta',
-  power: 7500
-}
-];
+  constructor( private dbzService: DbzService) {
 
-onNewCharacter( character: Character) : void {
-  console.log('Main Page Component');
-  console.log(character);
-}
+  }
+
+  get characters ():Character[] {
+    return [...this.dbzService.characters]
+  }
+
+  oneDeleteCharacter(id: string): void {
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter(character: Character): void {
+    this.dbzService.addCharacter(character);
+  }
 
 }
